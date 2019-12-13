tbg/benchdiff

commit 3b7fd0c76a1d20d15d6be1c12a308f43777156e0

s/benchcmp/benchdiff/
`benchcmp` was already taken: https://godoc.org/golang.org/x/tools/cmd/benchcmp.

It's not worth the confusion.

main.go
@@ -13,23 +13,23 @@ import (
 	"strings"
 	"time"
 
-	"github.com/nvanbenschoten/benchcmp/google"
-	"github.com/nvanbenschoten/benchcmp/ui"
+	"github.com/nvanbenschoten/benchdiff/google"
+	"github.com/nvanbenschoten/benchdiff/ui"
 	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
 	"golang.org/x/perf/benchstat"
 )
 
-const usage = `usage: benchcmp [--old <commit>] [--new <commit>] <pkgs>...`
+const usage = `usage: benchdiff [--old <commit>] [--new <commit>] <pkgs>...`
 
-const helpString = `benchcmp automates the process of running and comparing Go microbenchmarks
+const helpString = `benchdiff automates the process of running and comparing Go microbenchmarks
 across code changes.
 
-benchcmp runs all microbenchmarks in the specified packages against the old and
+benchdiff runs all microbenchmarks in the specified packages against the old and
 new commit. It then passes the benchmark output through benchstat to compute
 statistics about the results.
 
-By default, benchcmp outputs these results in a textual format. However, if the
+By default, benchdiff outputs these results in a textual format. However, if the
 --sheets flag is passed then it will upload the result to a Google Sheets
 spreadsheet. To access this, users must have a Google service account. For
 information, see https://cloud.google.com/iam/docs/service-accounts.
@@ -38,7 +38,7 @@ The Google service account must meet the following conditions:
 1. The Google Sheets API must be enabled for the account's project
 2. The Google Drive  API must be enabled for the account's project
 
-When the --sheets flag is passed, benchcmp will search for a credentials file
+When the --sheets flag is passed, benchdiff will search for a credentials file
 containing the service account key using the GOOGLE_APPLICATION_CREDENTIALS
 environment variable. See https://cloud.google.com/docs/authentication/production.
 
@@ -52,10 +52,10 @@ Options:
       --help            display this help
 
 Example invocations:
-  $ benchcmp --sheets ./pkg/...
-  $ benchcmp --old=master~ --new=master ./pkg/kv ./pkg/storage/...
-  $ benchcmp --new=d1fbdb2 --count=2 ./pkg/sql/...
-  $ benchcmp --new=6299bd4 --sheets --post-checkout='make buildshort' ./pkg/workload/...`
+  $ benchdiff --sheets ./pkg/...
+  $ benchdiff --old=master~ --new=master ./pkg/kv ./pkg/storage/...
+  $ benchdiff --new=d1fbdb2 --count=2 ./pkg/sql/...
+  $ benchdiff --new=6299bd4 --sheets --post-checkout='make buildshort' ./pkg/workload/...`
 
 // TODO: it's unclear whether G Suite Domain-wide Delegation is required for the
 // Google service account. If it is, add the following requirement to the help
@@ -120,7 +120,7 @@ func run(ctx context.Context) error {
 
 	// Run the benchmarks.
 	tests := oldSuite.intersectTests(&newSuite)
-	err = runbenchcmpes(ctx, &oldSuite, &newSuite, tests.sorted(), itersPerTest)
+	err = runbenchdiffes(ctx, &oldSuite, &newSuite, tests.sorted(), itersPerTest)
 	if err != nil {
 		return err
 	}
@@ -183,7 +183,7 @@ func buildBenches(ctx context.Context, pkgFilter []string, postChck string, bss 
 	return nil
 }
 
-func runbenchcmpes(ctx context.Context, bs1, bs2 *benchSuite, tests []string, itersPerTest int) error {
+func runbenchdiffes(ctx context.Context, bs1, bs2 *benchSuite, tests []string, itersPerTest int) error {
 	fmt.Println("\nrunning benchmarks:")
 	var spinner ui.Spinner
 	spinner.Start(os.Stdout, "")
@@ -211,7 +211,7 @@ func runbenchcmpes(ctx context.Context, bs1, bs2 *benchSuite, tests []string, it
 	return nil
 }
 
-func runbenchcmp(bs1, bs2 *benchSuite, test string) error {
+func runbenchdiff(bs1, bs2 *benchSuite, test string) error {
 	// Interleave test suite runs instead of using -count=itersPerTest. The
 	// idea is that this reduces the chance that we pick up external noise
 	// with a time correlation.
@@ -264,7 +264,7 @@ func processBenchOutput(ctx context.Context, bs1, bs2 *benchSuite, pkgFilter []s
 	tables := c.Tables()
 
 	if srv != nil {
-		name := fmt.Sprintf("benchcmp: %s (%s -> %s)", strings.Join(pkgFilter, " "), bs1.ref, bs2.ref)
+		name := fmt.Sprintf("benchdiff: %s (%s -> %s)", strings.Join(pkgFilter, " "), bs1.ref, bs2.ref)
 		url, err := srv.CreateSheet(ctx, name, tables)
 		if err != nil {
 			return err
@@ -297,20 +297,20 @@ func (bs *benchSuite) build(pkgFilter []string, postChck string, t time.Time) (e
 		panic("benchSuite already built")
 	}
 
-	// Create the artifacts directory: ./benchcmp/<ref>/artifacts
+	// Create the artifacts directory: ./benchdiff/<ref>/artifacts
 	bs.artDir = testArtifactsDir(bs.ref)
 	if err = os.MkdirAll(bs.artDir, 0744); err != nil {
 		return err
 	}
 
-	// Create output file: ./benchcmp/<ref>/artifacts/out.<time>
+	// Create output file: ./benchdiff/<ref>/artifacts/out.<time>
 	outFileName := bs.getOutputFile(t)
 	bs.outFile, err = os.OpenFile(outFileName, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		return err
 	}
 
-	// Create the binary directory: ./benchcmp/<ref>/bin/<hash(pkgFilter)>
+	// Create the binary directory: ./benchdiff/<ref>/bin/<hash(pkgFilter)>
 	bs.binDir = testBinDir(bs.ref, pkgFilter)
 	if _, err = os.Stat(bs.binDir); err == nil {
 		fmt.Printf("test binaries already exist for '%s'; skipping build\n", bs.ref)
@@ -332,7 +332,7 @@ func (bs *benchSuite) build(pkgFilter []string, postChck string, t time.Time) (e
 		return err
 	}
 	// If the binaries are not generated successfully, delete the bin directory
-	// so we don't consider the build successful next time benchcmp runs.
+	// so we don't consider the build successful next time benchdiff runs.
 	defer func() {
 		if err != nil {
 			_ = os.RemoveAll(bs.binDir)

bench.go
@@ -22,13 +22,13 @@ func expandPackages(pkgFilter []string) ([]string, error) {
 	return strings.Split(pkgs, "\n"), nil
 }
 
-// testDir returns the directory to store benchcmp artifacts and binaries for
+// testDir returns the directory to store benchdiff artifacts and binaries for
 // specified git ref.
 func testDir(ref string) string {
-	return filepath.Join("benchcmp", ref)
+	return filepath.Join("benchdiff", ref)
 }
 
-// testArtifactsDir returns the directory to store benchcmp artifacts for
+// testArtifactsDir returns the directory to store benchdiff artifacts for
 // specified git ref.
 func testArtifactsDir(ref string) string {
 	return filepath.Join(testDir(ref), "artifacts")
@@ -43,7 +43,7 @@ func hash(s []string) string {
 	return strconv.Itoa(int(u))
 }
 
-// testArtifactsDir returns the directory to store benchcmp binaries for
+// testArtifactsDir returns the directory to store benchdiff binaries for
 // specified git ref.
 func testBinDir(ref string, pkgFilter []string) string {
 	return filepath.Join(testDir(ref), "bin", hash(pkgFilter))

go.mod
@@ -1,9 +1,8 @@
-module github.com/nvanbenschoten/benchcmp
+module github.com/nvanbenschoten/benchdiff
 
 go 1.13
 
 require (
-	github.com/nvanbenschoten/cmpbench v0.0.0-20191212013857-bc027b208436
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/pflag v1.0.5
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45

go.sum
@@ -35,6 +35,8 @@ github.com/hashicorp/golang-lru v0.5.1 h1:0hERBMJE1eitiLkihrMvRVBYAkpHzc/J3QdDN+
 github.com/hashicorp/golang-lru v0.5.1/go.mod h1:/m3WP610KZHVQ1SGc6re/UDhFvYD7pJ4Ao+sR/qLZy8=
 github.com/jstemmer/go-junit-report v0.0.0-20190106144839-af01ea7f8024/go.mod h1:6v2b51hI/fHJwM22ozAgKL4VKDeJcHhJFhtBdhmNjmU=
 github.com/mattn/go-sqlite3 v0.0.0-20161215041557-2d44decb4941/go.mod h1:FPy6KqzDD04eiIsT53CuJW3U88zkxoIYsOqkbpncsNc=
+github.com/nvanbenschoten/benchcmp v0.0.0-20191212051423-b21755ba6bde h1:UvkYejK7VzX4EY4/riM4UR4xmCjFzTvodLsWgftNIW4=
+github.com/nvanbenschoten/benchcmp v0.0.0-20191212051423-b21755ba6bde/go.mod h1:Ygb2lEOmyAkY+0he6dTHw+EBz/TTBf6fyA15ua8JHas=
 github.com/nvanbenschoten/cmpbench v0.0.0-20191212013857-bc027b208436 h1:bQfQERQ+nc2cI3biS7hlEMmNa5b7cZ9P5kLyoXYOvC4=
 github.com/nvanbenschoten/cmpbench v0.0.0-20191212013857-bc027b208436/go.mod h1:01CAfYK7TrHTXC+/VapD7f9JA4P4j/0PlOOipkDwh3w=
 github.com/pkg/errors v0.8.1 h1:iURUrRGxPUNPdy5/HRSm+Yj6okJ6UtLINN0Q9M4+h3I=

commit 090f6301900d45938a058ef9b57fa4754725492a

Fix naming of runCmpBenches
This was broken in 3b7fd0c.

main.go
@@ -120,7 +120,7 @@ func run(ctx context.Context) error {
 
 	// Run the benchmarks.
 	tests := oldSuite.intersectTests(&newSuite)
-	err = runbenchdiffes(ctx, &oldSuite, &newSuite, tests.sorted(), itersPerTest)
+	err = runCmpBenches(ctx, &oldSuite, &newSuite, tests.sorted(), itersPerTest)
 	if err != nil {
 		return err
 	}
@@ -193,7 +193,7 @@ func buildBenches(ctx context.Context, pkgFilter []string, postChck string, bss 
 	return nil
 }
 
-func runbenchdiffes(ctx context.Context, bs1, bs2 *benchSuite, tests []string, itersPerTest int) error {
+func runCmpBenches(ctx context.Context, bs1, bs2 *benchSuite, tests []string, itersPerTest int) error {
 	fmt.Println("\nrunning benchmarks:")
 	var spinner ui.Spinner
 	spinner.Start(os.Stdout, "")
@@ -219,16 +219,6 @@ func runbenchdiffes(ctx context.Context, bs1, bs2 *benchSuite, tests []string, i
 		fmt.Println()
 	}
 	return nil
-}
-
-func runbenchdiff(bs1, bs2 *benchSuite, test string) error {
-	// Interleave test suite runs instead of using -count=itersPerTest. The
-	// idea is that this reduces the chance that we pick up external noise
-	// with a time correlation.
-	if err := runSingleBench(bs1, test); err != nil {
-		return err
-	}
-	return runSingleBench(bs2, test)
 }
 
 func runSingleBench(bs *benchSuite, test string) error {